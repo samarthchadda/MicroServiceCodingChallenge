504034016550c7fb88f88033e8c4490f3f7aa1e7
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2020-08-13 12:37:46 +0530

    Connected to Users Database

---
+++ IdentityService/users.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+
+//instance of express application
+const app = express();
+
+const bodyParser = require('body-parser');  //to save data from request
+
+app.use(bodyParser.json());
+
+
+//load mongoose
+const mongoose = require('mongoose');
+
+//connect to database
+mongoose.connect('mongodb+srv://samarth:YEhVsBnsh2nXlMa5@cluster0.ahjqa.mongodb.net/IdentityService?retryWrites=true&w=majority', { useUnifiedTopology: true })
+                                            .then(res=>{
+                                                console.log("connected to Database!!");
+                                            })
+                                            .catch(err=>console.log(err));
+
+
+app.listen(3000,()=>{
+    console.log("Up and running! -- This is our Identity service");
+});
+
+                                            
