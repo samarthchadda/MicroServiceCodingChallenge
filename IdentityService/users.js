c6e41c01877411596526906df90890a64b0199df
samarthchadda <samarthchadda.it20@jecrc.ac.in>
2020-08-13 12:54:20 +0530

    Post request for User

--- IdentityService/users.js
+++ IdentityService/users.js
@@ -23,6 +23,40 @@ mongoose.connect('mongodb+srv://samarth:YEhVsBnsh2nXlMa5@cluster0.ahjqa.mongodb.
                                             .catch(err=>console.log(err));
 
 
+app.get('/',(req, res, next)=>{
+    res.send("This is the Identity service!");
+})
+
+//'Create User' functionality
+app.post('/user',(req,res,next)=>{
+    
+    // console.log(req.body);   //printing JSON data send by post request
+
+    //saving book in our database
+    var newUser = {
+        FirstName:req.body.FirstName,
+        LastName:req.body.LastName,
+        Username:req.body.Username,
+        Password:req.body.Password,
+        Address:req.body.Address,
+        Age:req.body.Age,
+            
+    };
+
+    //model instance
+        //creating a new user with data from POST request
+    var user = new User(newUser);
+
+    console.log(user);
+
+    user.save().then(()=>{
+        console.log("New User Created!");
+    }).catch(err=>console.log(err));
+
+    res.send("User Created with success");
+
+})
+
 app.listen(3000,()=>{
     console.log("Up and running! -- This is our Identity service");
 });
